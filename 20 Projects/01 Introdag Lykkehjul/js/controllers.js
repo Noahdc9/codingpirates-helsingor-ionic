30ff83cc3de2f80ad761561312003d27ae7c1b8c
NielsGregers <niels@timeboxer.dk>
2016-01-19 18:21:01 +0100

    Introdag

---
+++ 20 Projects/01 Introdag Lykkehjul/js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('app.controllers', [])
+  
+.controller('lykkehjulCtrl', function($scope) {
+
+})
+   
+.controller('cartTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('cloudTabDefaultPageCtrl', function($scope) {
+
+})
+    
